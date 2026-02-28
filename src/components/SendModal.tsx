8721cc1bdc36bbc32aa2c16ac7c3d37153817609
Abdullrahman almaghrabi <73081830+abdprince@users.noreply.github.com>
2026-02-28 21:56:06 +0300

    Update SendModal.tsx

--- src/components/SendModal.tsx
+++ src/components/SendModal.tsx
@@ -7,8 +7,7 @@ interface Props {
   onSuccess: () => void
 }
 
-const FUNCTIONS_URL = 'https://wtlgiygdsgohibbpylln.supabase.co'
-
+const FUNCTIONS_URL = 'https://wtlgiygdsgohibbpylln.supabase.co/functions/v1'
 export default function SendModal({ userId, balance, onClose, onSuccess }: Props) {
   const [toId, setToId] = useState('')
   const [amount, setAmount] = useState('')
